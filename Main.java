12c8c643fe6fe5837c22d63f33d957b2b44f895f
AlexMarston2006 <alexander.marston2006@gmail.com>
2024-12-04 18:17:52 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        /*This simple program practices 
+         * printing things
+         */
+      System.out.println("Hello World");
+      System.out.println("I am learning Java");
+      System.out.println("It's pretty chill");
+      System.out.print("YOOOOOOOO ");
+      System.out.print("Waz gud?");
+      System.out.println(3);
+      System.out.println(358);
+      System.out.println(50000);
+      System.out.println(5+3);
+      System.out.println(5*3);
+    }
+  }
